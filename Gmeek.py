8131149b553c0c8c1a5680af0734f4715bbe4f39
Meekdai <meekdai@163.com>
2024-05-20 09:36:03 +0800

    修复文章description里包含双引号导致的BUG

--- Gmeek.py
+++ Gmeek.py
@@ -297,7 +297,7 @@ class GMEEK():
                         period="."
                 else:
                     period=self.blogBase["rssSplit"]
-                self.blogBase[listJsonName][postNum]["description"]=issue.body.split(period)[0]+period
+                self.blogBase[listJsonName][postNum]["description"]=issue.body.split(period)[0].replace("\"", "\'")+period
                 
             self.blogBase[listJsonName][postNum]["top"]=0
             for event in issue.get_events():
