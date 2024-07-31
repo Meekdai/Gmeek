82cd2aee9125161d7a2200b95874d4d9f1792c0f
Meekdai <meekdai@163.com>
2024-07-31 08:50:28 +0800

    Create GmeekVercount.js

---
+++ plugins/GmeekVercount.js
@@ -0,0 +1,16 @@
+function createBSZ() {
+    var postBody = document.getElementById('postBody');
+    if (postBody){
+        postBody.insertAdjacentHTML('afterend','<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">本文浏览量<span id="busuanzi_value_page_pv"></span>次</div>');
+    }
+    var runday = document.getElementById('runday');
+    runday.insertAdjacentHTML('afterend', '<span id="busuanzi_container_site_pv" style="display:none">总浏览量<span id="busuanzi_value_site_pv"></span>次 • </span>');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    createBSZ();
+    var element = document.createElement('script');
+    element.src = 'https://vercount.one/js';
+    document.head.appendChild(element);
+    console.log("\n %c GmeekBSZ Plugins https://github.com/Meekdai/Gmeek \n","padding:5px 0;background:#bc4c00;color:#fff");
+});
