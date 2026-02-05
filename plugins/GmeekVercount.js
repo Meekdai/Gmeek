77a8aaa2b1a5b5d4d5cbb7901ed8fc0559fd5ed4
Meekdai <meekdai@163.com>
2026-02-05 13:27:12 +0800

    Update GmeekVercount.js

--- plugins/GmeekVercount.js
+++ plugins/GmeekVercount.js
@@ -1,10 +1,10 @@
 function createVercount() {
     var postBody = document.getElementById('postBody');
     if (postBody){
-        postBody.insertAdjacentHTML('afterend','<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">本文浏览量<span id="busuanzi_value_page_pv"></span>次</div>');
+        postBody.insertAdjacentHTML('afterend','<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">本文浏览量 <span id="busuanzi_value_page_pv"></span> 次</div>');
     }
     var runday = document.getElementById('runday');
-    runday.insertAdjacentHTML('afterend', '<span id="busuanzi_container_site_pv" style="display:none">总浏览量<span id="busuanzi_value_site_pv"></span>次 • </span>');
+    runday.insertAdjacentHTML('afterend', '<span id="busuanzi_container_site_pv" style="display:none">总浏览量 <span id="busuanzi_value_site_pv"></span> 次 • </span>');
 }
 
 document.addEventListener("DOMContentLoaded", function() {
